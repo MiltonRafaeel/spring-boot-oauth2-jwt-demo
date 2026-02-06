976a85e4898a07b9efe96e4f8f860a7d6a6b49cc
MiltonRafael <miltonrafael166@gmail.com>
2026-02-06 17:55:51 -0300

    criando uma criptografia na senha e comparando com o hash

--- password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,28 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner{
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("ENCODE = " + passwordEncoder.encode("123456"));
+		
+		//como o spring compara a senha 123456 com o hash gerado ? dessa forma: 
+		boolean result = passwordEncoder.matches("123456", "$2a$10$uzN6bKF99n7Z4vps0uJQRurgkVKA8AKepxqpqFv82JvlFrTpcgYsO");
+		System.out.println("RESULTADO DA COMPARACAO = " + result);
 	}
 
 }
--- password-grant/spring-boot-3-4-3-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-4-3-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$uzN6bKF99n7Z4vps0uJQRurgkVKA8AKepxqpqFv82JvlFrTpcgYsO');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$uzN6bKF99n7Z4vps0uJQRurgkVKA8AKepxqpqFv82JvlFrTpcgYsO');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
